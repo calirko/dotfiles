6d37eb21a6414427cf48fcb50a47fcc57503cc3f
calirko <me@calirko.com>
2026-09-02 09:16:48 -0300

    fixes and improvements

--- configs/eww/scripts/watch-clock.sh
+++ configs/eww/scripts/watch-clock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-    printf '{"time":"%s","date":"%s"}\n' "$(date +'%H:%M')" "$(date +'%a, %d %b %Y')"
+    printf '{"time":"%s","date":"%s"}\n' "$(date +'%-I:%M %p')" "$(date +'%a, %d %b %Y')"
     sleep 1
 done
